59971099a437a33c228a1225db43c763c17d4dbf
J370 <jetong2001@gmail.com>
2020-01-15 15:49:25 +0800

    Fixing logo

--- js/agency.js
+++ js/agency.js
@@ -35,3 +35,7 @@ $('div.modal').on('show.bs.modal', function() {
 		}
 	}
 });
+
+$(document).ready(function() {
+    $(window).scrollTop(0);
+});
--- thanks.md
+++ thanks.md
@@ -1,6 +1,7 @@
 ---
 layout: convey
 permalink: /thanks.html
+sitemap: false
 ---
 
 Thank you for your message, you will hear from me soon!
