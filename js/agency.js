9dd6d554b6f3090f5666018b74b7f83dd2a6ece1
J370 <jetong2001@gmail.com>
2020-03-27 17:28:36 +0800

    Scroll padding

--- js/agency.js
+++ js/agency.js
@@ -15,6 +15,12 @@ $(function() {
     });
 });
 
-$(document).ready(function() {
-    $(window).scrollTop(0);
+$(document).on("scroll", function(){
+    if($(document).scrollTop() > 100){
+      $("header").addClass("shrink");
+    }
+    else
+    {
+        $("header").removeClass("shrink");
+    }
 });
