ac829f11a57aa0868b8482e4875b05061e840a69
J370 <jetong2001@gmail.com>
2020-03-27 10:45:32 +0800

    Remove toggle

--- js/agency.js
+++ js/agency.js
@@ -25,17 +25,6 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-$('div.modal').on('show.bs.modal', function() {
-	var modal = this;
-	var hash = modal.id;
-	window.location.hash = hash;
-	window.onhashchange = function() {
-		if (!location.hash){
-			$(modal).modal('hide');
-		}
-	}
-});
-
 $(document).ready(function() {
     $(window).scrollTop(0);
 });
