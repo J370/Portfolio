6a4876f02568361d11ec3916463c8ed2e93ad4cf
J370 <jetong2001@gmail.com>
2020-04-30 11:26:33 +0800

    On load aos

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,2 +1,27 @@
-$(function(){$('a.page-scroll').bind('click',function(event){var $anchor=$(this);$('html, body').stop().animate({scrollTop:$($anchor.attr('href')).offset().top},1500,'easeInOutExpo');event.preventDefault()})});$(document).on("scroll",function(){if($(document).scrollTop()>100){$(".navbar-default").addClass("shrink")}
-else{$(".navbar-default").removeClass("shrink")}});$(".page-scroll").click(function(){$('.collapse').collapse("hide")})
+$(function () {
+  $("a.page-scroll").bind("click", function (event) {
+    var $anchor = $(this);
+    $("html, body")
+      .stop()
+      .animate(
+        { scrollTop: $($anchor.attr("href")).offset().top },
+        1500,
+        "easeInOutExpo"
+      );
+    event.preventDefault();
+  });
+});
+$(document).on("scroll", function () {
+  if ($(document).scrollTop() > 100) {
+    $(".navbar-default").addClass("shrink");
+  } else {
+    $(".navbar-default").removeClass("shrink");
+  }
+});
+$(".page-scroll").click(function () {
+  $(".collapse").collapse("hide");
+});
+$(document).on("load", function() {
+    //Animate on scroll
+    AOS.init();
+})
