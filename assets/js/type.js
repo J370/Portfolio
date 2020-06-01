610bd78c7138da90c23d4f38a3ff4083309b5e08
Jet Ong <j370@pop-os.localdomain>
2020-06-01 14:56:55 +0800

    Lighthouse fixes

--- assets/js/type.js
+++ assets/js/type.js
@@ -2,4 +2,4 @@ var TxtType=function(el,toRotate,period){this.toRotate=toRotate;this.el=el;this.
 this.el.innerHTML='<span class="wrap">'+this.txt+'</span>';var that=this;var delta=200-Math.random()*100;if(this.isDeleting){delta/=2}
 if(!this.isDeleting&&this.txt===fullTxt){delta=this.period;this.isDeleting=!0}else if(this.isDeleting&&this.txt===''){this.isDeleting=!1;this.loopNum++;delta=500}
 setTimeout(function(){that.tick()},delta)};window.onload=function(){var elements=document.getElementsByClassName('typewrite');for(var i=0;i<elements.length;i++){var toRotate=elements[i].getAttribute('data-type');var period=elements[i].getAttribute('data-period');if(toRotate){new TxtType(elements[i],JSON.parse(toRotate),period)}}
-var css=document.createElement("style");css.type="text/css";css.innerHTML=".typewrite > .wrap { border-right: 0.08em solid #fff}";document.body.appendChild(css)}
+var css=document.createElement("style");css.type="text/css";css.innerHTML=".typewrite > .wrap { border-right: 0.08em solid #000}";document.body.appendChild(css)}
